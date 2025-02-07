d71fa0d893ccdf144b8a2ec10bec2a10e102902e
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-07 15:49:06 +0700

    feat(chat-event): add handlers for ChatCreated and ChatUpdated events

---
+++ packages/chat/src/application/event/handler/chat-created-handler.ts
@@ -0,0 +1,38 @@
+import type { IEventHandler } from "@ai-ctx/core";
+import { ChatCreated } from "@src/domain/event/chat-created";
+import { TYPES } from "@src/types";
+import type { Client } from "cassandra-driver";
+import { inject } from "inversify";
+import { Logger } from "winston";
+
+export class ChatCreatedEventHandler implements IEventHandler<ChatCreated> {
+  public event = ChatCreated.name;
+
+  constructor(
+    @inject(TYPES.CassandraDb) private readonly _cassandraCli: Client,
+    @inject(TYPES.Logger) private readonly _logger: Logger
+  ) {}
+
+  async handle(event: ChatCreated): Promise<void> {
+    const query = `INSERT INTO chat (guid, content, role, images, tool_calls, version) VALUES (?, ?, ?, ?, ?, ?)`;
+
+    await this._cassandraCli.execute(
+      query,
+      [
+        event.guid,
+        event.content,
+        event.role,
+        event.images,
+        event.tool_calls,
+        event.version,
+      ],
+      { prepare: true }
+    );
+
+    this._logger.info(
+      `created read model for chat - ChatCreatedEventHandler: ${JSON.stringify(
+        event
+      )}`
+    );
+  }
+}
---
+++ packages/chat/src/application/event/handler/chat-updated-handler.ts
@@ -0,0 +1,31 @@
+import type { IEventHandler } from "@ai-ctx/core";
+import { ChatUpdated } from '../../../domain/event/chat-updated';
+import { TYPES } from "@src/types";
+
+import type { Client } from "cassandra-driver";
+import { inject } from "inversify";
+export class ChatUpdatedEventHandler implements IEventHandler<ChatUpdated> {
+    public event = ChatUpdated.name;
+
+    constructor(
+        @inject(TYPES.CassandraDb) private readonly _cassandraCli: Client,
+    ) {}
+    
+    async handle(event: ChatUpdated): Promise<void> {
+        const query = `UPDATE chat SET content = ?, role = ?, images = ?, tool_calls = ? WHERE guid = ?`;
+ 
+        await this._cassandraCli.execute(
+            query,
+            [
+                event.content,
+                event.role,
+                event.images,
+                event.tool_calls,
+                event.guid,
+            ],
+            { prepare: true }
+        )
+    }
+    public cmdToHandler = "chat.updated";
+    
+}
