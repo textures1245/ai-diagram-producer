0b844f973fcfaf0851cadefa7749dc51b7bd4035
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-18 17:23:21 +0700

    feat(setup): update Cassandra setup script to create tables for users and workspaces with additional fields

--- setup/cassandra.sh
+++ setup/cassandra.sh
@@ -6,4 +6,10 @@ done
 
 echo "Initialising Cassandra ..."
 cqlsh cassandra -e "CREATE KEYSPACE IF NOT EXISTS ai_ctx_dev WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.chats (guid text PRIMARY KEY, content text, role text, images text, tool_calls text, version int);"
+
+# using in analyzer domain
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.chats (guid text, user_guid text, workspace_guid text, content text, role text, images text, tool_calls text, version int, PRIMARY KEY ((guid), user_guid, workspace_guid);"
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.workspaces (guid text, user_guid text, created_at timestamp, version int, PRIMARY KEY ((guid), user_guid);"
+
+# using in presenter domain
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.users (guid text , name text, created_at timestamp, updated_at timestamp, version int, PRIMARY KEY (guid));"
