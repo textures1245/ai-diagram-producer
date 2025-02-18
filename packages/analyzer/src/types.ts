5b91dd78802e92b97e1f4507e475d2d8bb8fa2a1
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-18 17:20:28 +0700

    feat(analyzer-types): add WorkspaceRepository and related query handlers to TYPES

--- packages/analyzer/src/types.ts
+++ packages/analyzer/src/types.ts
@@ -1,22 +1,31 @@
 export const TYPES = {
-    Db: Symbol('Db'),
-    CassandraDb: Symbol('CassandraDb'),
-    KafkaProducer: Symbol('KafkaProducer'),
-    KafkaConsumer: Symbol('KafkaConsumer'),
-    RedisSubscriber: Symbol('RedisSubscriber'),
-    Redis: Symbol('Redis'),
-    EventBus: Symbol('EventBus'),
-    CommandBus: Symbol('CommandBus'),
-    QueryBus: Symbol('QueryBus'),
-    CommandHandler: Symbol('CommandHandler'),
-    QueryHandler: Symbol('QueryHandler'),
-    Event: Symbol('Event'),
-    EventHandler: Symbol('EventHandler'),
-    EventStore: Symbol('EventStore'),
-    ChatRepository: Symbol('ChatRepository'),
-    ChatEventStore: Symbol('ChatEventStore'),
-    GetAllChatsQueryHandler: Symbol('GetAllChatsQueryHandler'),
-    ApiServer: Symbol('ApiServer'),
-    Logger: Symbol('Logger'),
-  };
-  
+  Db: Symbol("Db"),
+  CassandraDb: Symbol("CassandraDb"),
+  KafkaProducer: Symbol("KafkaProducer"),
+  KafkaConsumer: Symbol("KafkaConsumer"),
+  RedisSubscriber: Symbol("RedisSubscriber"),
+  Redis: Symbol("Redis"),
+  EventBus: Symbol("EventBus"),
+  CommandBus: Symbol("CommandBus"),
+  QueryBus: Symbol("QueryBus"),
+  CommandHandler: Symbol("CommandHandler"),
+  QueryHandler: Symbol("QueryHandler"),
+  Event: Symbol("Event"),
+  EventHandler: Symbol("EventHandler"),
+  EventStore: Symbol("EventStore"),
+
+  ApiServer: Symbol("ApiServer"),
+  Logger: Symbol("Logger"),
+
+  // analyzer package
+  ChatRepository: Symbol("ChatRepository"),
+  ChatEventStore: Symbol("ChatEventStore"),
+  GetAllChatsQueryHandler: Symbol("GetAllChatsQueryHandler"),
+  GetChatsByWorkspaceIdQueryHandler: Symbol("GetChatsByWorkspaceIdQueryHandler"),
+
+  WorkspaceRepository: Symbol("WorkspaceRepository"),
+  WorkspaceEventStore: Symbol("WorkspaceEventStore"),
+  GetWorkspacesByUserIdQueryHandler: Symbol(
+    "GetWorkspacesByUserIdQueryHandler"
+  ),
+};
