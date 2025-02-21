d2352c054fd0fdc9a74b1a759c457b9ac9188b73
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-21 18:35:37 +0700

    feat(presenter-routes): add AuthRoute and CoreRoute components with authentication and chat functionality

---
+++ packages/presenter/src/lib/CoreRoute.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Chatbox from "./../../../../node_modules/@ai-ctx/presenter/src/lib/route-components/core/Chatbox.svelte";
+  import Navbar from "./layout-components/Navbar.svelte";
+  import { workspacesHistories } from "./mock/data";
+
+  let contents = {
+    username: "textures1245",
+    workspaceHistories: workspacesHistories,
+  };
+</script>
+
+<div id="container" class="bg-muted/30">
+  <main class="flex flex-col">
+    <header>
+      <Navbar {contents} />
+      <hr class="bg-black text-black w-full" />
+    </header>
+    <div class="h-96">
+      <Chatbox />
+    </div>
+  </main>
+  <footer class=""></footer>
+</div>
