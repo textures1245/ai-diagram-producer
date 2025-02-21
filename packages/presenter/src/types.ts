10d0d9ca24560f54cf55a1269737b702d66cf2db
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-21 18:36:13 +0700

    chore(presenter): add Tailwind CSS configuration and styles, update package dependencies, and introduce component types

---
+++ packages/presenter/src/types.ts
@@ -0,0 +1,4 @@
+export const TYPES = {
+  JwtSecret: Symbol("JwtSecret"),
+  GoogleToken: Symbol("GoogleToken"),
+};
--- packages/presenter/wxt.config.ts
+++ packages/presenter/wxt.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  extensionApi: "chrome",
+  modules: ["@wxt-dev/module-svelte"],
+  alias: {
+    $lib: "./src/lib",
+  },
+
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        $lib: "./src/lib",
+      },
+    },
+    build: {
+      target: "esnext",
+    },
+  }),
 });
