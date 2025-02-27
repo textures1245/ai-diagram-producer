564a11d73fdf05b967375843eb4cbbb488a373fd
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-02-27 20:00:19 +0700

    feat(presenter-backend-infrastructure): implement command bus, query bus, and event bus with MongoDB and Cassandra integration

---
+++ packages/presenter/src/backend/infrastructure/db/mongodb.ts
@@ -0,0 +1,13 @@
+import config from "@backend/config";
+import { type MongoClientOptions, MongoClient, Db } from "mongodb";
+
+export const createMongodbConn = async (
+  hostUrl: string,
+  opts?: MongoClientOptions
+): Promise<Db> => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(hostUrl, opts)
+      .then((client) => resolve(client.db(config.DB_NAME)))
+      .catch((err) => reject(err));
+  });
+};
---
+++ packages/presenter/src/backend/infrastructure/event-bus/kafka.ts
@@ -0,0 +1,48 @@
+import {
+  type IEventBus,
+  type IEvent,
+  type IEventHandler,
+  type EventDescriptor,
+  rehydrateEventFromDescriptor,
+} from "@ai-ctx/core";
+import { TYPES } from "@backend/types";
+import { injectable, multiInject, inject } from "inversify";
+import type { Consumer, Producer } from "kafkajs";
+import { instanceToPlain } from "class-transformer";
+
+@injectable()
+export class KafkaEventBus implements IEventBus {
+  constructor(
+    @multiInject(TYPES.Event)
+    private readonly eventHandlers: IEventHandler<IEvent>[],
+    @inject(TYPES.KafkaConsumer) private readonly _subscriber: Consumer,
+    @inject(TYPES.KafkaProducer) private readonly _producer: Producer
+  ) {}
+  async publish(chan: string, event: EventDescriptor): Promise<void> {
+    const payload: string = JSON.stringify({ ...instanceToPlain(event) });
+
+    await this._producer.send({
+      topic: chan,
+      messages: [{ value: payload, key: event.aggregateGuid }],
+    });
+  }
+  async subscribeEvents(): Promise<void> {
+    await this._subscriber.run({
+      eachMessage: async ({ message, heartbeat }) => {
+        if (message.value) {
+          const eventDescriptor = JSON.parse(message.value.toString());
+          const matchedHandlers: IEventHandler<IEvent>[] =
+            this.eventHandlers.filter((handler) => {
+              return handler.event === eventDescriptor.type;
+            });
+          await Promise.all(
+            matchedHandlers.map((handler: IEventHandler<IEvent>) => {
+              handler.handle(rehydrateEventFromDescriptor(eventDescriptor));
+            })
+          );
+          await heartbeat();
+        }
+      },
+    });
+  }
+}
