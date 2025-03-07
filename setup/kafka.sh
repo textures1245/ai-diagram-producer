54f0db85c2deb41fa13240f61e5030be373c5822
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-03-07 20:04:14 +0700

    migrate(setup): migrate cassandra and kafka schema

--- setup/kafka.sh
+++ setup/kafka.sh
@@ -3,6 +3,11 @@ kafka-topics --bootstrap-server kafka:29092 --list
         
 echo -e "Creating topics ..."
 kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic chat --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic workspace --replication-factor 1 --partitions 1
+kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic user --replication-factor 1 --partitions 1
 
 echo -e "Successfully created topics:"
 kafka-topics --bootstrap-server kafka:29092 --list
+
+#TEMP for debug consumer only
+# kafka-console-consumer --bootstrap-server localhost:9092 --topic user --from-beginning
