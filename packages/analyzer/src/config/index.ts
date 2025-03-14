d5bb379926ce1cb0485fc0796cd6ded8303bb252
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-03-14 18:55:06 +0700

    update(analyzer-config): update Kafka consumer group ID for analyzer environment and ensure proper connection and subscription handling

--- packages/analyzer/src/config/index.ts
+++ packages/analyzer/src/config/index.ts
@@ -5,7 +5,7 @@ export default {
   REDIS_URI: process.env["REDIS_URI"] || "redis://localhost:6379",
   KAFKA_BROKER_LIST: process.env["KAFKA_BROKER_LIST"] || "localhost:9092",
   KAFKA_CONSUMER_GROUP_ID:
-    process.env["KAFKA_CONSUMER_GROUP_ID"] || "ai_ctx.dev",
+    process.env["KAFKA_CONSUMER_GROUP_ID"] || "ai_ctx.analyzer.dev",
   KAFKA_TOPICS_TO_SUBSCRIBE: process.env["KAFKA_TOPICS_TO_SUBSCRIBE"] || "chat,workspace",
   CASSANDRA_HOSTS: process.env["CASSANDRA_HOSTS"]
     ? process.env["CASSANDRA_HOSTS"].split(",")
--- packages/analyzer/src/subscribers/index.ts
+++ packages/analyzer/src/subscribers/index.ts
@@ -2,7 +2,7 @@ import "@api/http/controller/index";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import 'reflect-metadata';
+import "reflect-metadata";
 import type { IEventBus } from "@ai-ctx/core";
 import config from "@src/config";
 import { infraInitialize } from "@src/startup";
@@ -11,13 +11,16 @@ import type { Consumer } from "kafkajs";
 
 (async () => {
   const container = await infraInitialize();
+
   const kafkaConsumer = container.get<Consumer>(TYPES.KafkaConsumer);
-  kafkaConsumer.connect();
+
+  await kafkaConsumer.connect();
 
   for (const topic of config.KAFKA_TOPICS_TO_SUBSCRIBE.split(",")) {
-    await kafkaConsumer.subscribe({ topic });
+    await kafkaConsumer.subscribe({ topic, fromBeginning: true });
   }
 
   const baseEventHandler = container.get<IEventBus>(TYPES.EventBus);
-  baseEventHandler.subscribeEvents();
+
+  await baseEventHandler.subscribeEvents();
 })();
