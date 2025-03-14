f5ffffd9c7ce5dc21a59795950fe3713be8f8591
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-03-14 19:04:08 +0700

    feat(setup): enhance Cassandra schema by adding timestamps to chats and workspaces tables

--- setup/cassandra.sh
+++ setup/cassandra.sh
@@ -8,12 +8,12 @@ echo "Initialising Cassandra ..."
 cqlsh cassandra -e "CREATE KEYSPACE IF NOT EXISTS ai_ctx_dev WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
 
 # using in analyzer domain
-cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.chats (guid text, user_guid text, workspace_guid text, content text, role text, images text, tool_calls text, version int, PRIMARY KEY ((guid), user_guid, workspace_guid));"
-cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.workspaces (guid text, user_guid text, created_at timestamp, version int, PRIMARY KEY ((guid), user_guid));"
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.chats (guid text, user_guid text, workspace_guid text, content text, role text, images text, tool_calls text, version int, created_at timestamp, updated_at timestamp, PRIMARY KEY ((guid), user_guid, workspace_guid));"
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.workspaces (guid text, user_guid text, title text, created_at timestamp, updated_at timestamp, version int, PRIMARY KEY ((guid), user_guid));"
 
 # using in presenter domain
-cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.users (guid text, email text, password text, created_at timestamp, updated_at timestamp, version int, PRIMARY KEY ((guid), email);"
-cqlsh cassandra -e "CREATE INDEX ON ai_ctx_dev.users(email);"
+cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.users (guid text, email text, password text, created_at timestamp, updated_at timestamp, version int, PRIMARY KEY ((guid), email));"
+cqlsh cassandra -e "CREATE INDEX IF NOT EXISTS ON ai_ctx_dev.users(email);"
 
 # For the two-table approach (optional but recommended)
 # cqlsh cassandra -e "CREATE TABLE IF NOT EXISTS ai_ctx_dev.users_by_email (email text, guid text, password text, created_at timestamp, updated_at timestamp, version int, PRIMARY KEY (email));"
